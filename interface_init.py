50c1fec81f30e3aae4fb5f85a8976a221f6b8ab1
Aleksei <odvansgame@gmail.com>
2022-03-21 17:58:17 +0200

    Update

--- interface_init.py
+++ interface_init.py
@@ -1,16 +1,17 @@
 import tkinter
 from tkinter import *
-from tkinter import Label
+from tkinter import Label, messagebox
 import cv2
 import threading
 
-from os import listdir
-from os.path import isfile, join
+from os import listdir, mkdir
+from os.path import isfile, join, isdir
 
 from PIL import Image, ImageTk
 
 from pipeline_exec import Pipeline
 from pipeline_creation import CreatePipeline
+
 
 class App:
     def __init__(self):
@@ -43,61 +44,78 @@ class App:
         self.pipeline_list = OptionMenu(self.root, self.value, *self.pipeline_names, command=self.execute_pipeline)
         self.pipeline_list.grid(row=0, column=0, sticky='nw', padx=(100, 0), pady=(25, 25))
 
-        self.create_new_pipeline_button = Button(self.root, text='Create new pipeline', command=self.open_name_new_pipeline_window)
+        self.create_new_pipeline_button = Button(self.root, text='Create new pipeline',
+                                                 command=self.open_name_new_pipeline_window)
         self.create_new_pipeline_button.grid(row=0, column=0, sticky='nw', padx=(250, 0), pady=(27, 0))
 
     def open_name_new_pipeline_window(self):
         self.create_pipeline_name = tkinter.Toplevel(self.root)
         self.create_pipeline_name.title('Create Pipeline Name')
 
-        self.label1 = tkinter.Label(self.create_pipeline_name, text='Please type pipeline name')
+        self.label1 = tkinter.Label(self.create_pipeline_name, text='Please enter pipeline name')
         self.label1.pack(pady=(10, 0))
 
         self.text_field = tkinter.Entry(self.create_pipeline_name, width=50)
         self.text_field.pack(padx=(50, 50), pady=(10, 10))
 
-        self.close_button = tkinter.Button(self.create_pipeline_name, text='Cancel', command=self.create_pipeline_name.destroy)
+        self.close_button = tkinter.Button(self.create_pipeline_name, text='Cancel',
+                                           command=self.create_pipeline_name.destroy)
         self.close_button.pack(side=LEFT, padx=(150, 0), pady=(0, 15))
 
-        self.start_button = tkinter.Button(self.create_pipeline_name, text='Start', command=lambda: self.open_create_pipeline_window(self.text_field.get(), self.create_pipeline_name))
+        self.start_button = tkinter.Button(self.create_pipeline_name, text='Start',
+                                           command=lambda: self.open_create_pipeline_window(self.text_field.get(),
+                                                                                            self.create_pipeline_name))
         self.start_button.pack(side=LEFT, padx=(10, 0), pady=(0, 15))
 
-
-
     def open_create_pipeline_window(self, name, previous_window):
-        previous_window.destroy()
-        self.new_pipeline_name = name
+        if self.text_field.get() != "" and self.create_new_pipeline_folder():
+            previous_window.destroy()
+            self.new_pipeline_name = name
 
-        self.create_pipeline = tkinter.Toplevel(self.root)
-        self.create_pipeline.title('Create Pipeline')
+            self.create_pipeline = tkinter.Toplevel(self.root)
+            self.create_pipeline.title('Create Pipeline')
 
-        self.action_label = tkinter.Label(self.create_pipeline, text='Select actions')
-        self.action_label.grid(row=0, column=0)
+            self.action_label = tkinter.Label(self.create_pipeline, text='Select actions')
+            self.action_label.grid(row=0, column=0)
 
-        self.action_listbox = tkinter.Listbox(self.create_pipeline)
+            self.action_listbox = tkinter.Listbox(self.create_pipeline)
 
-        for i, entry in enumerate(self.actions_list):
-            self.action_listbox.insert(i, entry)
-        self.action_listbox.grid(row=1, column=0)
+            for i, entry in enumerate(self.actions_list):
+                self.action_listbox.insert(i, entry)
+            self.action_listbox.grid(row=1, column=0)
 
-        self.param_label = tkinter.Label(self.create_pipeline, text='Select parameters')
-        self.param_label.grid(row=0, column=1)
+            self.param_label = tkinter.Label(self.create_pipeline, text='Select parameters')
+            self.param_label.grid(row=0, column=1)
 
-        self.action_parameters_listbox = tkinter.Listbox(self.create_pipeline)
-        self.action_parameters_listbox.grid(row=1, column=1)
+            self.action_parameters_listbox = tkinter.Listbox(self.create_pipeline)
+            self.action_parameters_listbox.grid(row=1, column=1)
 
-        self.sequence_label = tkinter.Label(self.create_pipeline, text='Current sequence')
-        self.sequence_label.grid(row=2, column=0)
+            self.sequence_label = tkinter.Label(self.create_pipeline, text='Current sequence')
+            self.sequence_label.grid(row=2, column=0)
 
-        self.sequence_listbox = tkinter.Listbox(self.create_pipeline)
-        self.sequence_listbox.grid(row=3, column=0)
+            self.sequence_listbox = tkinter.Listbox(self.create_pipeline)
+            self.sequence_listbox.grid(row=3, column=0)
 
-        self.execute_sequence_button = tkinter.Button(self.create_pipeline, text='Execute', command=lambda: [self.create_pipeline.destroy(), CreatePipeline(self.task_sequence, self.root)])
-        self.execute_sequence_button.grid(row=3, column=1)
+            self.execute_sequence_button = tkinter.Button(self.create_pipeline, text='Execute',
+                                                          command=lambda: [self.create_pipeline.destroy(),
+                                                                           CreatePipeline(self.task_sequence, self.root, self.new_pipeline_name)])
+            self.execute_sequence_button.grid(row=3, column=1)
 
-        self.action_listbox.bind('<Double-Button-1>', lambda x: self.open_add_task_window(self.action_listbox, self.sequence_listbox))
-        self.sequence_listbox.bind('<Double-Button-1>', lambda x: self.change_action_parameters(self.sequence_listbox, self.action_parameters_listbox))
-        self.action_parameters_listbox.bind('<Double-Button-1>', lambda x: self.action_parameters_processing(self.action_parameters_listbox, self.sequence_listbox))
+            self.action_listbox.bind('<Double-Button-1>',
+                                     lambda x: self.open_add_task_window(self.action_listbox, self.sequence_listbox))
+            self.sequence_listbox.bind('<Double-Button-1>',
+                                       lambda x: self.change_action_parameters(self.sequence_listbox,
+                                                                               self.action_parameters_listbox))
+            self.action_parameters_listbox.bind('<Double-Button-1>', lambda x: self.action_parameters_processing(
+                self.action_parameters_listbox, self.sequence_listbox))
+
+    def create_new_pipeline_folder(self):
+        if not isdir('pipelines/{}'.format(self.text_field.get())):
+            mkdir('pipelines/{}'.format(self.text_field.get()))
+            return True
+        else:
+            messagebox.showerror('Error', 'Pipeline name is already taken')
+            return False
 
     def open_add_task_window(self, listbox1, listbox2):
         self.add_task_window = tkinter.Toplevel(self.root)
@@ -106,7 +124,9 @@ class App:
         self.close_button = tkinter.Button(self.add_task_window, text='Close', command=self.add_task_window.destroy)
         self.close_button.pack(side=LEFT)
 
-        self.add_button = tkinter.Button(self.add_task_window, text='Add', command=lambda: [self.add_task(listbox1, listbox2), self.add_task_window.destroy()])
+        self.add_button = tkinter.Button(self.add_task_window, text='Add',
+                                         command=lambda: [self.add_task(listbox1, listbox2),
+                                                          self.add_task_window.destroy()])
         self.add_button.pack(side=LEFT)
 
     def add_task(self, listbox1, listbox2):
--- pipeline_creation.py
+++ pipeline_creation.py
@@ -1,12 +1,14 @@
 import queue
-import threading
+from worker import worker
 import tkinter
 import cv2
-from PIL import Image, ImageTk
+import os
+from PIL import Image, ImageTk, ImageGrab
 
 
 class CreatePipeline:
-    def __init__(self, pipeline, root):
+    def __init__(self, pipeline, root, pipeline_name):
+        self.image_listbox_image_id = None
         self.boxes_listbox = None
         self.thread = None
         self.finish_button = None
@@ -16,9 +18,8 @@ class CreatePipeline:
         self.image_listbox = None
 
         self.pipeline_sequence = pipeline
+        self.pipeline_name = pipeline_name
         self.root = root
-
-        self.boxes_cords = []
 
         self.canvas = None
 
@@ -31,6 +32,7 @@ class CreatePipeline:
         self.rects = {}
 
         self.rect = None
+        self.original_rects = None
 
         self.rect_count = 0
 
@@ -39,6 +41,18 @@ class CreatePipeline:
         self.curX = self.curY = 0
 
         self.cap = cv2.VideoCapture(0)
+
+        self.isNamingWindowOpened = False
+
+        self.image_count = 0
+
+        self.current_image = 0
+
+        self.classes = {}
+
+        self.sv = tkinter.StringVar()
+
+        self.isTakingSnapshot = True
 
         self.terminal()
 
@@ -53,6 +67,7 @@ class CreatePipeline:
 
         self.image_listbox = tkinter.Listbox(self.labeling_window, height=40)
         self.image_listbox.grid(row=0, column=0, rowspan=5)
+        self.image_listbox.bind('<Double-Button-1>', lambda x: self.switch_image_alert())
 
         self.canvas = tkinter.Canvas(self.labeling_window, width=1280, height=720)
         self.canvas.grid(row=0, column=1, rowspan=5)
@@ -65,14 +80,15 @@ class CreatePipeline:
         self.boxes_listbox.grid(row=0, column=2)
         self.boxes_listbox.bind('<Double-Button-1>', lambda x: self.delete_box(self.boxes_listbox))
 
-        self.take_snapshot_button = tkinter.Button(self.labeling_window, text='Take snapshot', command=self.take_snapshot)
+        self.take_snapshot_button = tkinter.Button(self.labeling_window, text='Take snapshot',
+                                                   command=self.take_snapshot)
         self.take_snapshot_button.grid(row=1, column=2)
 
         self.abort_button = tkinter.Button(self.labeling_window, text='Abort', command=self.abort_snapshot)
         self.abort_button['state'] = 'disabled'
         self.abort_button.grid(row=2, column=2)
 
-        self.next_button = tkinter.Button(self.labeling_window, text='Next image')
+        self.next_button = tkinter.Button(self.labeling_window, text='Next image', command=self.next_image)
         self.next_button['state'] = 'disabled'
         self.next_button.grid(row=3, column=2)
 
@@ -80,94 +96,341 @@ class CreatePipeline:
         self.finish_button['state'] = 'disabled'
         self.finish_button.grid(row=4, column=2)
 
-        self.thread = threading.Thread(target=self.update_canvas)
-        self.thread.start()
+        self.thread = self.update_canvas()
 
     def delete_box(self, listbox):
-        index = int(listbox.get(listbox.curselection()[0]).split(' ')[1][1:])
+        listbox_item = listbox.get(listbox.curselection()[0])
+
+        index = int(listbox_item.split(' ')[1][1:])
         self.canvas.delete(self.rects[index][0])
         self.canvas.delete(self.rects[index][1])
         listbox.delete(listbox.curselection()[0])
+
+        class_name = listbox_item.split(' - ')[1]
+
+        self.classes[class_name] -= 1
+
+        if self.classes[class_name] == 0:
+            del self.classes[class_name]
+
         del self.rects[index]
 
     def on_button_press(self, event):
-        if self.abort_button['state'] == 'normal':
-            # inside_box = False
-            # for cords in self.boxes_cords:
-            #     if
+        if self.abort_button['state'] == 'normal' and not self.isNamingWindowOpened:
             self.start_x = event.x
             self.start_y = event.y
 
             self.rect = self.canvas.create_rectangle(self.x, self.y, 0, 0, outline="green", width=2)
 
     def on_move_press(self, event):
-        self.curX, self.curY = (event.x, event.y)
+        if self.abort_button['state'] == 'normal' and not self.isNamingWindowOpened:
+            self.curX, self.curY = (event.x, event.y)
 
-        self.canvas.coords(self.rect, self.start_x, self.start_y, self.curX, self.curY)
+            self.canvas.coords(self.rect, self.start_x, self.start_y, self.curX, self.curY)
 
     def on_button_release(self, event):
-        if self.abort_button['state'] == 'normal':
+        if self.abort_button['state'] == 'normal' and not self.isNamingWindowOpened:
+            self.isNamingWindowOpened = True
             naming_window = tkinter.Toplevel(self.root)
             naming_window.title('Name box')
 
+            self.root.register(self.disable_enable_btn_by_entry)
+
+            self.sv = tkinter.StringVar()
+
             self.entry = tkinter.Entry(naming_window)
             self.entry.grid(row=0, column=0, columnspan=2)
+            self.sv.trace('w', lambda nm, idx, mode, var=self.sv: self.disable_enable_btn_by_entry(var))
+            self.entry.config(textvariable=self.sv)
+            self.entry.focus()
 
-            cancel_button = tkinter.Button(naming_window, text='Cancel', command=lambda: [self.remove_rect(self.rect), naming_window.destroy()])
+            cancel_button = tkinter.Button(naming_window, text='Cancel',
+                                           command=lambda: [self.remove_rect(self.rect), naming_window.destroy()])
             cancel_button.grid(row=1, column=0)
 
-            ok_button = tkinter.Button(naming_window, text='Ok', command=lambda: [self.add_rect(event), naming_window.destroy()])
-            ok_button.grid(row=1, column=1)
+            self.ok_button = tkinter.Button(naming_window, text='Ok',
+                                       command=lambda: [self.add_rect(event), naming_window.destroy()])
+            self.ok_button.grid(row=1, column=1)
+
+            self.ok_button['state'] = 'disabled'
+
+    def disable_enable_btn_by_entry(self, value):
+        if len(value.get()) == 0:
+            self.ok_button['state'] = 'disabled'
+        else:
+            self.ok_button['state'] = 'normal'
 
     def remove_rect(self, rect):
         self.canvas.delete(rect)
 
     def add_rect(self, event):
-        top_left = [self.curX if self.start_x > self.curX else self.start_x, self.curY if self.start_y > self.curY else self.start_y]
-        bottom_right = [self.curX if self.start_x < self.curX else self.start_x, self.curY if self.start_y < self.curY else self.start_y]
 
-        self.rects[self.rect_count] = [self.rect, self.canvas.create_text(top_left[0] + 50, top_left[1] - 20, fill='green', font='Helvetica 12 bold', text='Box #{} - {}'.format(self.rect_count, self.entry.get())), [top_left[0], top_left[1], bottom_right[0], bottom_right[1]]]
+        top_left = [self.curX if self.start_x > self.curX else self.start_x,
+                    self.curY if self.start_y > self.curY else self.start_y]
+        bottom_right = [self.curX if self.start_x < self.curX else self.start_x,
+                        self.curY if self.start_y < self.curY else self.start_y]
+
+        self.rects[self.rect_count] = [self.rect,
+                                       self.canvas.create_text(top_left[0] + 50, top_left[1] - 20, fill='green',
+                                                               font='Helvetica 12 bold',
+                                                               text='Box #{} - {}'.format(self.rect_count,
+                                                                                          self.entry.get())),
+                                       [top_left[0], top_left[1], bottom_right[0], bottom_right[1]], self.entry.get()]
+        if self.entry.get() in self.classes:
+            self.classes[self.entry.get()] += 1
+        else:
+            self.classes[self.entry.get()] = 1
+
         self.boxes_listbox.insert(tkinter.END, 'Box #{} - {}'.format(self.rect_count, self.entry.get()))
         self.rect_count += 1
+        self.isNamingWindowOpened = False
 
     def take_snapshot(self):
         self.take_snapshot_button['state'] = 'disabled'
         self.abort_button['state'] = 'normal'
         self.next_button['state'] = 'normal'
 
+        self.current_image = self.image_count
+
         self.rects = {}
 
-        self.q.put(False)
+        self.image_listbox.insert(tkinter.END, 'Image: #{}'.format(self.image_count))
+
+        self.save_image()
+
+        self.isTakingSnapshot = True
+
+        self.thread.abort()
 
     def abort_snapshot(self):
+        # Disabling Abort and Next buttons while user changing positions of details on table
         self.take_snapshot_button['state'] = 'normal'
         self.abort_button['state'] = 'disabled'
         self.next_button['state'] = 'disabled'
 
+        # Changing Save button text to Next image and Delete button to Abort
+        self.next_button['text'] = 'Next image'
+        self.abort_button['text'] = 'Abort'
+
+        # Deleting already existing boxes
+        for key, value in self.rects.items():
+            self.canvas.delete(value[0])
+            self.canvas.delete(value[1])
+
+        # Clearing self.rects dictionary
         self.rects = {}
 
-        self.q.put(True)
+        # Setting self.rect_count to 0
+        self.rect_count = 0
+
+        # Getting all boxes on the image
+        boxes = self.boxes_listbox.get(0, tkinter.END)
+
+        # Iterating through all boxes in the image
+        for item in boxes:
+
+            # Getting class name of current box
+            class_name = item.split(' - ')[1]
+
+            # Decreasing current class boxes count
+            self.classes[class_name] -= 1
+
+            # Deleting current class if there is no boxes with such class
+            if self.classes[class_name] == 0:
+                del self.classes[class_name]
+
+        # Deleting current image
+        self.delete_image(self.current_image)
+
+        # Also deleting label if user is deleting already existing image
+        if self.image_count != self.current_image:
+            self.delete_label(self.current_image)
+
+            # Deleting current image from image listbox
+            self.image_listbox.delete(self.image_listbox_image_id)
+        else:
+            self.image_listbox.delete(self.image_listbox.get(0, tkinter.END).index('Image: #{}'.format(self.image_count)))
+
+        # Clearing boxes listbox
+        self.boxes_listbox.delete(0, tkinter.END)
+
+        # Continuing to stream webcam image on canvas
+        self.thread = self.update_canvas()
+
+        self.isTakingSnapshot = False
 
     def next_image(self):
+        # Disabling Abort and Next buttons while user changing positions of details on table
         self.take_snapshot_button['state'] = 'normal'
         self.abort_button['state'] = 'disabled'
         self.next_button['state'] = 'disabled'
 
-        self.q.put(True)
+        self.save_image_data()
 
+        # Changing label data in existing label file
+        if self.next_button['text'] == 'Next image':
+            # Increasing image counter
+            self.current_image += 1
+            self.image_count += 1
 
+        # Zeroing rectangle count
+        self.rect_count = 0
 
+        # Clearing boxes listbox
+        self.boxes_listbox.delete(0, tkinter.END)
+
+        # Continuing to stream webcam image on canvas
+        self.thread = self.update_canvas()
+
+        # Changing Save button text to Next image and Delete button to Abort
+        self.next_button['text'] = 'Next image'
+        self.abort_button['text'] = 'Abort'
+
+        self.isTakingSnapshot = False
+
+    def delete_image(self, image):
+        os.remove('pipelines/{}/data/temp_data/images/img_{}.png'.format(self.pipeline_name, image))
+
+    def delete_label(self, label):
+        os.remove('pipelines/{}/data/temp_data/labels/label_{}.txt'.format(self.pipeline_name, label))
+
+    def save_image(self):
+        if not os.path.isdir('pipelines/{}/data/temp_data'.format(self.pipeline_name)):
+            os.mkdir('pipelines/{}/data'.format(self.pipeline_name))
+            os.mkdir('pipelines/{}/data/temp_data'.format(self.pipeline_name))
+            os.mkdir('pipelines/{}/data/temp_data/images'.format(self.pipeline_name))
+            os.mkdir('pipelines/{}/data/temp_data/labels'.format(self.pipeline_name))
+
+        frame = self.cap.read()
+
+        cv2.imwrite('pipelines/{}/data/temp_data/images/img_{}.png'.format(self.pipeline_name, self.image_count),
+                    frame[1])
+
+    def save_image_data(self):
+        # Opening current label file
+        with open('pipelines/{}/data/temp_data/labels/label_{}.txt'.format(self.pipeline_name, self.current_image),
+                  'w') as file:
+            # Starting loop to write each box to file
+            for key, value in self.rects.items():
+                # Getting class name but not it's ID because image classes could change during labeling
+                class_name = value[3]
+
+                # Bringing X1Y1X2Y2 coordinates to form that YOLO model could use it
+                center_x = int((value[2][0] + value[2][2]) / 2) / 1280
+                center_y = int((value[2][1] + value[2][3]) / 2) / 720
+                width = int(value[2][2] - value[2][0]) / 1280
+                height = int(value[2][3] - value[2][1]) / 720
+
+                # Writing current line to file
+                file.write('{} {} {} {} {} \n'.format(class_name, center_x, center_y, width, height))
+
+        self.isTakingSnapshot = False
+
+    def switch_image_alert(self):
+        if (self.image_count != self.current_image and self.rects != self.original_rects) or self.isTakingSnapshot:
+            save_menu = tkinter.Toplevel(self.root)
+            save_menu.title('Alert!')
+
+            label = tkinter.Label(save_menu, text='Are you sure you want to leave unsaved image?')
+            label.grid(row=0, column=0, columnspan=3)
+
+            tkinter.Button(save_menu, text='Save', command=lambda: [self.save_image_data(), self.switch_image(), save_menu.destroy()]).grid(row=1, column=0)
+            if self.isTakingSnapshot:
+                tkinter.Button(save_menu, text='Leave', command=lambda: [self.abort_snapshot(), self.switch_image(), save_menu.destroy()]).grid(row=1, column=1)
+            else:
+                tkinter.Button(save_menu, text='Leave', command=lambda: [self.switch_image(), save_menu.destroy()]).grid(row=1, column=1)
+
+            tkinter.Button(save_menu, text='Cancel', command=save_menu.destroy).grid(row=1, column=2)
+
+            self.root.wait_window(save_menu)
+        else:
+            self.switch_image()
+
+    def switch_image(self):
+        # Making Save and Delete buttons active while Taking Snapshot
+        self.take_snapshot_button['state'] = 'disabled'
+        self.abort_button['state'] = 'normal'
+        self.next_button['state'] = 'normal'
+
+        # Changing Next button text to Save and Abort button to Delete
+        self.next_button['text'] = 'Save'
+        self.abort_button['text'] = 'Delete'
+
+        # Stopping live stream from webcam
+        self.thread.abort()
+
+        # Deleting already existing boxes
+        for key, value in self.rects.items():
+            self.canvas.delete(value[0])
+            self.canvas.delete(value[1])
+
+        # Clearing boxes list
+        self.boxes_listbox.delete(0, tkinter.END)
+
+        # Clearing rects dictionary
+        self.rects = {}
+
+        # Getting id of image that user was clicked on
+        image_id = int(self.image_listbox.get(self.image_listbox.curselection()[0]).split(' ')[1][1:])
+
+        # Setting current image id to class variable self.current_image so other methods could use that information
+        self.current_image = image_id
+        
+        self.image_listbox_image_id = self.image_listbox.curselection()[0]
+
+        # Changing image in the canvas
+        image = cv2.imread('pipelines/{}/data/temp_data/images/img_{}.png'.format(self.pipeline_name, image_id))
+        img = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+        imgTk = ImageTk.PhotoImage(image=img)
+        self.canvas.create_image(0, 0, image=imgTk, anchor=tkinter.NW)
+
+        # Opening current image label file
+        label = open('pipelines/{}/data/temp_data/labels/label_{}.txt'.format(self.pipeline_name, image_id), 'r')
+
+        # Initializing index that represent id of box in image
+        index = 0
+
+        # Starting while loop
+        while label:
+            # Reading new line
+            line = label.readline()
+
+            # Exiting loop if line is empty
+            if line == "":
+                break
+
+            # Splitting received row to separate pieces
+            line = line.split(' ')
+
+            # Restoring coordinates to pixels
+            x1 = int(float(line[1]) * 1280) - int((float(line[3]) / 2) * 1280)
+            y1 = int(float(line[2]) * 720) - int((float(line[4]) / 2) * 720)
+            x2 = int(float(line[1]) * 1280) + int((float(line[3]) / 2) * 1280)
+            y2 = int(float(line[2]) * 720) + int((float(line[4]) / 2) * 720)
+
+            # Adding box and label to image and at the same moment adding those objects to self.rects dictionary
+            self.rects[index] = [self.canvas.create_rectangle(x1, y1, x2, y2, outline="green", width=2),
+                                 self.canvas.create_text(x1 + 50, y1 - 20, fill='green',
+                                                         font='Helvetica 12 bold',
+                                                         text='Box #{} - {}'.format(index, line[0])),
+                                 [x1, y1, x2, y2],
+                                 line[0]]
+            self.boxes_listbox.insert(tkinter.END, 'Box #{} - {}'.format(index, line[0]))
+
+            # Increasing index by 1
+            index += 1
+
+        self.original_rects = self.rects
+
+        self.isTakingSnapshot = False
+
+    @worker
     def update_canvas(self):
         while True:
-            if self.q.qsize() > 0:
-                val = self.q.get()
-            while val:
-                if self.q.qsize() > 0:
-                    val = self.q.get()
-                _, self.frame = self.cap.read()
+            _, self.frame = self.cap.read()
 
-                self.img = Image.fromarray(cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB))
-                self.imgTk = ImageTk.PhotoImage(image=self.img)
+            self.img = Image.fromarray(cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB))
+            self.imgTk = ImageTk.PhotoImage(image=self.img)
 
-                self.canvas.create_image(0, 0, image=self.imgTk, anchor=tkinter.NW)
-                self.canvas.image = self.imgTk
+            self.canvas.create_image(0, 0, image=self.imgTk, anchor=tkinter.NW)
+            self.canvas.image = self.imgTk
